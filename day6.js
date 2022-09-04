a2bc378fb9b524a6ea8b1eec6e8583fcdf6f8c9f
Reemamaiman <110451179+Reemamaiman@users.noreply.github.com>
2022-09-04 23:35:38 +0300

    reema

---
+++ day6.js
@@ -0,0 +1,24 @@
+const input = document.getElementById('name-input');
+const button = document.getElementById('search-button');
+const gender = document.getElementById('gender');
+button.addEventListener('click', async () => {
+  const value = input.value;
+
+try{
+  const request=await fetch(
+      `https://api.genderize.io/?name=${value}`
+  );
+  if (request.status!=200){
+    return;
+  }
+  const data=await request.json();
+
+  console.log(data)
+gender.innerHTML=data.gender
+}
+catch (error){
+console.log(error);
+
+}
+
+});
